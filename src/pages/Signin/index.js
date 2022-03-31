ed8ae0f58bd4c5c256f17984149a46f1c54c86e8
Jailson Prado <jailsondpd@gmail.com>
2022-03-31 00:04:35 -0300

    criando page de login e aplicando styled components

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,10 +1,48 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from "./styles";
+
 export default function Signin() {
   return (
-    <View>
-      <Text>Signin</Text>
-    </View>
+    <Background>
+      <Container>
+        <Logo source={require("../../assets/Logo.png")} />
+
+        <AreaInput>
+          <Input
+            placeholder="Email"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+        </AreaInput>
+
+        <AreaInput>
+          <Input
+            placeholder="Senha"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+        </AreaInput>
+
+        <SubmitButton>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link>
+          <LinkText>Criar uma conta!</LinkText>
+        </Link>
+      </Container>
+    </Background>
   );
 }
